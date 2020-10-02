b1c0acfc739230072b181cd0c75ae9bf2a98ab84
habbe <91habbe@gmail.com>
2020-10-02 14:45:03 +0200

    added Hab namespace, active records. updated navbar

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,3 +1,4 @@
-INSERT INTO User ("username", "password") VALUES("asdf", "asdf");
+INSERT INTO User ("username", "password") VALUES("BashLord", "password1");
+INSERT INTO Tag ("name") VALUES("asdf");
 
 SELECT * FROM User;
